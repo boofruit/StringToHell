c0d8a171e5e5cdec4fd2e999b11a65c0221b3350
kurisaka_konabe <k.kurisaka@gmail.com>
2026-06-16 14:22:01 +0900

    風のスクリプトの使っていなかった無駄な行を削除

--- Assets/String to Hell/Test/KurisakaTest/WindUVScroller.cs
+++ Assets/String to Hell/Test/KurisakaTest/WindUVScroller.cs
@@ -3,24 +3,19 @@ using static UnityEngine.InputManagerEntry;
 
 public class WindUVScroller : MonoBehaviour
 {
-    [SerializeField] private Color windColor = Color.white;
     [SerializeField, Range(0.001f, 0.04f)] private float windPowerRate = 0.01f;
     [SerializeField] private QuadWind[] winds;
-    //private Material[] mats;
 
     private float windPower;
     private Vector2 windDir;
-    //private Material mat;
     private AreaEffector2D wind;
 
     void Start()
-    {
-        //mat = GetComponent<Renderer>().material;   
+    { 
         wind = GetComponent<AreaEffector2D>();
         windDir = AngleToDir(wind);
         windPower = wind.forceMagnitude * windPowerRate;
 
-        //mats = new Material[winds.Length];
         for (int i = 0; i < winds.Length; i++)
         {
             winds[i].Mat = winds[i].quad.material;
